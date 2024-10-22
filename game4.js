57658840b4c52637c962e5f2ce38008945636e7d
Oksana-sky <oksanakozhemyakaok@gmail.com>
2024-10-22 23:41:40 +0300

    Homework-8

--- game4.js
+++ game4.js
@@ -1,6 +1,79 @@
 const button4 = document.getElementById('myGame4');
 
+const randommIndex = () => Math.floor(Math.random() * 3);
+
 button4.onclick = startGame;
 
+let check, a, b;
+
 function startGame() {
+
+    const answers = [
+        'камень',
+        'ножницы',
+        'бумага'
+    ];
+
+    let answerComp = answers[randommIndex()];
+
+    let sec = true;
+    let userChoice;
+
+    do {
+        userChoice = prompt('Выбери:\nкамень, ножницы, бумага');
+
+        userChoice = userChoice.toLowerCase();
+
+        for (let i = 0; i < 3; i++) {
+            if (userChoice === answers[i]) {
+                sec = false;
+            };
+        };
+        if (sec) {
+            alert('Ошибка, попробуйте еще раз!');
+        };
+    } while (sec);
+
+    for (let i = 0; i < 3; i++) {
+        if (answerComp === answers[i]) {
+            a = i;
+        };
+        if (userChoice === answers[i]) {
+            b = i;
+        };
+    };
+    /*
+    a - компьютер
+    b - игрок
+    0 - камень
+    1 - ножницы
+    2 - бумага
+    */
+    if (a === b) {
+        check = 2;
+    } else if (a === 0 && b === 1) {
+        check = 0;
+    } else if (a === 0 && b === 2) {
+        check = 1;
+    } else if (a === 1 && b === 2) {
+        check = 0;
+    } else if (a === 1 && b === 0) {
+        check = 1;
+    } else if (a === 2 && b === 0) {
+        check = 0;
+    } else if (a === 2 && b === 1) {
+        check = 1;
+    };
+
+    console.log(check);
+
+    alert(`Ваш выбор: ${userChoice}\n Выбор компьютера: ${answerComp}`);
+
+    if (check === 1) {
+        alert('Вы победили');
+    } else if (check === 0) {
+        alert('Вы проиграли');
+    } else if (check === 2) {
+        alert('Ничья');
+    };
 }
---
+++ hw-2.8.js
@@ -0,0 +1,95 @@
+//Задание 1
+
+/* const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+
+ console.log(people.sort((a,b) => a.age - b.age)); */
+
+//Задание 2
+
+/* function isPositive(num) {
+    return num >= 0;
+}
+
+function isMale(person) {
+    return person.gender === "male";
+}
+
+function filter(array, ruleFunction) {
+    const result = [];
+    for (let item of array) {
+        if (ruleFunction(item)) {
+            result.push(item);
+        };
+    };
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [{
+        name: 'Глеб',
+        gender: 'male'
+    },
+    {
+        name: 'Анна',
+        gender: 'female'
+    },
+    {
+        name: 'Олег',
+        gender: 'male'
+    },
+    {
+        name: 'Оксана',
+        gender: 'female'
+    }
+];
+
+console.log(filter(people, isMale)); */
+
+//Задание 3
+
+/* function logCurrentDate() {
+    const currentDate = new Date();
+    console.log(`Текущая дата и время: ${currentDate.toLocaleString('ru-Ru')}`);
+};
+
+const intervalId = setInterval(logCurrentDate, 3000);
+
+const STOP_TIME = 30 * 1000;
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("30 секунд прошли");
+}, STOP_TIME); */
+
+//Задание 4
+
+/* function delayForSecond(calback){
+    setTimeout (calback, 1000);
+};
+
+delayForSecond(function () {
+    console.log('Привет, Глеб!');
+ }); */
+
+//Задание 5
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) {
+            cb();
+        }
+    }, 1000)
+};
+
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+};
+
+delayForSecond(() => sayHi('Глеб'));
